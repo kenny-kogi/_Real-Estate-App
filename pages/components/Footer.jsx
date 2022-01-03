57e5b71f67942ba377da8f09421bc41eca5cd389
kenny-kogi <mwangiken497@gmail.com>
2022-01-03 16:55:48 +0300

    implement home with chakra ui

---
+++ pages/components/Footer.jsx
@@ -0,0 +1,16 @@
+import { Box } from "@chakra-ui/react";
+
+const Footer = () => {
+  return (
+    <Box
+      borderTop="1px"
+      textAlign="center"
+      color="gray.600"
+      borderColor="gray.200"
+    >
+      Realtor 2022 | Inc.
+    </Box>
+  );
+};
+
+export default Footer;
